e3c2e86f2eba19ec89e0ed66d208ef487fd3aee1
mpyw <ryosuke_i_628@yahoo.co.jp>
2022-07-13 06:15:47 +0900

    refactor: 💡 Simplify logic

--- src/AdvisoryLocks.php
+++ src/AdvisoryLocks.php
@@ -9,23 +9,9 @@ use Illuminate\Database\Connection;
 use Illuminate\Database\QueryException;
 use Mpyw\LaravelDatabaseAdvisoryLock\Contracts\LockerFactory as FactoryContract;
 
-use function property_exists;
-
 trait AdvisoryLocks
 {
     public ?FactoryContract $advisoryLocker = null;
-
-    /**
-     * @param  string         $query
-     * @param  array          $bindings
-     * @throws QueryException
-     */
-    abstract protected function tryAgainIfCausedByLostConnection(
-        QueryException $e,
-        $query,
-        $bindings,
-        Closure $callback,
-    );
 
     public function advisoryLocker(): FactoryContract
     {
@@ -42,10 +28,9 @@ trait AdvisoryLocks
     protected function handleQueryException(QueryException $e, $query, $bindings, Closure $callback)
     {
         assert($this instanceof Connection);
-        assert(property_exists($this, 'transactions'));
 
         // Don't try again if there are session-level locks
-        if ((int)$this->transactions >= 1 || $this->advisoryLocker()->forSession()->hasAny()) {
+        if ($this->transactionLevel() > 0 || $this->advisoryLocker()->forSession()->hasAny()) {
             throw $e;
         }
 
